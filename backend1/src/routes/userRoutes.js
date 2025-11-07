f3e9fb5f349d94b9a40a86143ca871e37fc1c628
Pedro Rodrigues Almeida <pedrominato23@gmail.com>
2025-11-07 19:05:48 -0300

    Adicionando frontend

--- backend1/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getUserById } = require('../controllers/userController');
-const { checkToken } = require('../middlewares/auth');
-
-const router = express.Router();
-
-router.get('/:id', checkToken, getUserById);
-
-module.exports = router;
